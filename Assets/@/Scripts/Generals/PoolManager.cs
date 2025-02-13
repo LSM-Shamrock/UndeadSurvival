f6972e8a498499e2c179ea00d9b6aa7ffdffc944
LSM-Shamrock <lsm.shamrock@gmail.com>
2025-02-13 16:58:05 +0900

    2025-02-13

--- Assets/@/Scripts/Generals/PoolManager.cs
+++ Assets/@/Scripts/Generals/PoolManager.cs
@@ -3,19 +3,21 @@ using UnityEngine;
 
 public static class PoolManager 
 {
-    private static Transform _poolParent;
     private static Dictionary<string, Queue<GameObject>> _pool = new();
-
-    private static Transform GetPoolParent()
+    private static Transform _poolParent;
+    private static Transform PoolParent
     {
-        if (_poolParent == null)
+        get
         {
-            var go = new GameObject("Pool Parent");
-            GameObject.DontDestroyOnLoad(go);
-            go.SetActive(false);
-            _poolParent = go.transform;
+            if (_poolParent == null)
+            {
+                var go = new GameObject("Pool Parent");
+                GameObject.DontDestroyOnLoad(go);
+                go.SetActive(false);
+                _poolParent = go.transform;
+            }
+            return _poolParent;
         }
-        return _poolParent;
     }
 
     public static GameObject CreateFromPool(GameObject prefab)
@@ -26,7 +28,7 @@ public static class PoolManager
         var go = pool.Count > 0 ? pool.Dequeue() : null;
         if (go == null)
         {
-            go = GameObject.Instantiate(prefab, GetPoolParent());
+            go = GameObject.Instantiate(prefab, PoolParent);
             go.name = key;
         }
         go.transform.SetParent(null);
@@ -35,7 +37,7 @@ public static class PoolManager
 
     public static void RemoveToPool(GameObject target)
     {
-        target.transform.SetParent(GetPoolParent());
+        target.transform.SetParent(PoolParent);
         var key = target.name;
         if (!_pool.ContainsKey(key)) _pool.Add(key, new());
         _pool[key].Enqueue(target);
